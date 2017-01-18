403d628141774db434b1ff70dba7cc680cc3eabf
PatrykPiatek <punek666@gmail.com>
2017-01-18 22:03:53 +0100

    umiem liczyć

---
+++ hello.py
@@ -0,0 +1,34 @@
+# cos = ["ryuk" + " rajk"]
+# cos.append("text") 
+# range(1,10)
+
+
+# def petla(liczba):
+# 	for x in cos:
+# 		for y in range (1,liczba):
+# 			print(str(y) + str(x) +" huhu" )
+# 	pass
+# 	pass
+
+# petla(10)
+
+lista = []
+for x in range(1,101):
+	lista.append(x)
+	pass
+print(lista)
+
+lista2 = []
+for element in lista:
+	lista2.append(element*5)
+print(lista2)
+
+lista3 = [] 
+for cos in lista2:
+	lista3.append(cos/4)
+print(lista3)
+
+lista4 = [] 
+for cos in lista3:
+	lista4.append(cos*5)
+print(lista4)
